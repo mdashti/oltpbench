29d4329a0d45e521419c5b96d5a0ea08007cbe6b
Mohammad Dashti <mdashti@gmail.com>
2016-02-09 20:20:35 +0100

    Updated TATP script

--- run/runTATP.sh
+++ run/runTATP.sh
@@ -1,1 +1,1 @@
-java -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b tatp -c config/sample_tatp_config.xml --execute true
+java  -Dlog4j.configuration=./log4j.properties -Xmx1024m -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b tatp -c config/sample_tatp_config.xml --execute true --clear true --load true
